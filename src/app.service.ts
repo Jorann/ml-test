1ec3793b742fa4efaef13e8d14eefd8850ba52e9
Joran de Waaij <joran@lifely.nl>
2021-08-16 11:12:19 +0200

    feat(tfjs): make tfjs work and sample code

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,29 @@
 import { Injectable } from '@nestjs/common';
 
+import * as tf from '@tensorflow/tfjs-node';
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  async getHello(): Promise<string> {
+    const model = tf.sequential();
+    model.add(
+      tf.layers.dense({ units: 100, activation: 'relu', inputShape: [10] }),
+    );
+    model.add(tf.layers.dense({ units: 1, activation: 'linear' }));
+    model.compile({ optimizer: 'sgd', loss: 'meanSquaredError' });
+
+    const xs = tf.randomNormal([100, 10]);
+    const ys = tf.randomNormal([100, 1]);
+
+    let out = {};
+    const h = await model.fit(xs, ys, {
+      epochs: 100,
+      callbacks: {
+        onEpochEnd: (epoch, log) => {
+          out = { epoch, log };
+        },
+      },
+    });
+
+    return JSON.stringify(out);
   }
 }
